6826e37bbbfcc095211ce47fe8e78ce45513f306
Sameer <sameer.barha12@gmail.com>
2023-12-04 12:52:52 +0530

    updated duration formatting

--- durationcheck.py
+++ durationcheck.py
@@ -1,33 +1,26 @@
 from pathlib import Path
 import argparse
 import subprocess32 as subprocess
-import math
 import os
 import sys
+import mimetypes
+mimetypes.init()
+import emoji
 
-def format_time(seconds):
+def durationFormat(duration, full=False):
     TOTAL_MIN=0
     TOTAL_SEC=0
-    
-    TOTAL_SEC += int(seconds%60)
-    TOTAL_MIN += int(seconds/60) + int(TOTAL_SEC/60)
+    TOTAL_SEC += int(duration%60)
+    TOTAL_MIN += int(duration/60) + int(TOTAL_SEC/60)
     TOTAL_SEC = int(TOTAL_SEC%60)
 
-    return "{:.1f}hr {}min {}secs ".format(math.floor(TOTAL_MIN/60), TOTAL_MIN%60, TOTAL_SEC)
-
+    if full==True:
+        return "{}hr {}min {}secs {} ".format(int(TOTAL_MIN/60), TOTAL_MIN%60, TOTAL_SEC, emoji.emojize(":check_mark_button:"))
+    else:
+        return " {}mins {}secs {}".format(TOTAL_MIN%60, TOTAL_SEC, emoji.emojize(":check_mark_button:"))
+    #print("Total Duration: {}hr {}min {}secs ".format(TOTAL_MIN/60, TOTAL_MIN%60, TOTAL_SEC))
 
 def getDuration(filename):
-
-    command = [
-        'ffprobe',
-        '-v',
-        'error',
-        '-show_entries',
-        'format=duration',
-        '-of',
-        'default=noprint_wrappers=1:nokey=1',
-        filename
-      ]
 
     try:
         filename =str(filename)
@@ -38,12 +31,21 @@ def getDuration(filename):
         stderr=subprocess.STDOUT)
 
     except subprocess.CalledProcessError as e:
+
         output = float(output.stdout)
-        
-    if output.stdout.decode("utf-8") =="N/A":
+
+    if output.stdout.decode("utf-8") == 'N/A':
         return '0.0'
     else:
         return output.stdout.decode("utf-8") 
+
+def checkMimeType(file_path):
+
+    mimestart = mimetypes.guess_type(file_path)[0]
+    if mimestart != None:
+        return True if mimestart.split('/')[0]=='audio' or mimestart.split('/')[0]=='video' else False
+    else:
+        return False
 
 
 def folderDuration(folderPath):
@@ -53,37 +55,41 @@ def folderDuration(folderPath):
     for path in Path(folderPath).iterdir():
         info = path.stat()
         if os.path.isdir(str(path)):
-            curr_scope = float(folderDuration(path)) 
+            curr_scope = float(folderDuration(path))
             duration += curr_scope
-            
-            print("{}/ --> {}".format( str(path).replace(str(folderPath),''), format_time(curr_scope)) )
-        elif str(path).endswith('.mp4') or str(path).endswith('.avi') or str(path).endswith('.ts'):
-            curr_scope = float(getDuration(path)) 
+            print("{}/ --> {}\n".format( path.name, durationFormat(curr_scope)) )
+
+        elif checkMimeType(str(path)):
+            curr_scope = float(getDuration(path))
             duration += curr_scope
-            #print("{} --> {}".format( path, curr_scope) )
-        
+						#Individual File
+            print("{} --> {}\n".format( path, durationFormat(curr_scope)) )
+
     return duration
 
 
 if __name__=="__main__":
-    
+
     ap = argparse.ArgumentParser()
     ap.add_argument("-f", "--path", required=True, help=" \"path\" to target folder")
     args = vars(ap.parse_args())
-    
+
     FOLDER_PATH = str(args["path"])
-    
+
     print("\nScanning Folder :\n{} ... \n".format(FOLDER_PATH))
-    
+
     if not os.path.isdir(FOLDER_PATH):
         print("** Please enter a valid Folder Path **")
         exit()
-        
+
     elif os.path.exists(os.path.dirname(FOLDER_PATH))==False:
         print("** This folder path does not exist **")
         exit()
-    
-    duration = folderDuration(FOLDER_PATH)
-    print(duration,' sec')
 
-    print("Total Duration: {}  ".format(format_time(duration)))
+    TOTAL_MIN=0
+    TOTAL_SEC=0
+
+    duration = folderDuration(FOLDER_PATH)
+    print("{} minutes".format(int(duration/60)))
+
+    print( "{} Total Duration: {} \n".format(emoji.emojize(":check_mark_button:"), durationFormat(duration, True))) 
